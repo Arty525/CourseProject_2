9f4e3ee6c4c3bf2dd4b5aa8d36ce56d930cd5ed5
Андрей <newart525@yandex.ru>
2025-03-07 16:12:51 +0300

    test for read_file 50% ready

--- tests/test_file_editor.py
+++ tests/test_file_editor.py
@@ -40,19 +40,9 @@ def vacancies():
     ]
     return vacancies
 
+@patch("json.load")
 @patch("builtins.open")
-def test_read_file(vacancies):
+def test_read_file(mock_open, mock_json_load, vacancies):
+    mock_json_load.return_value = vacancies
     json_editor = JSONEditor()
-    mock_load = Mock(return_value=vacancies)
-    json.load = mock_load.return_value
-    assert (json_editor.read_file("", {"keyword": "Python", "salary": 10000}) ==
-            [{
-                "id": "117961997",
-                "name": "Python разработчик",
-                "salary": None,
-                "url": "https://api.hh.ru/vacancies/123",
-                "snippet": {
-                    "requirement": "Python, Git, Docker",
-                    "responsibility": "Разработка бэк-энда"
-                }
-            }])
+    assert json_editor.read_file("", {"keyword": "Разработчик", "salary": 10000}) == [vacancies[1]]
